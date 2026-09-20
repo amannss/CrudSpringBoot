64349564b9a40b3f8cf46418799f44b55641573a
= <amansharma96609@gmail.com>
2026-09-20 20:31:22 +0530

    added CRUD operations - post , get , getall , update , delete using JPA methods

--- CrudSpringBootDemo/src/main/java/in/amanarmy/CrudSpringBootDemo/service/StudentService.java
+++ CrudSpringBootDemo/src/main/java/in/amanarmy/CrudSpringBootDemo/service/StudentService.java
@@ -5,6 +5,9 @@ import in.amanarmy.CrudSpringBootDemo.repository.StudentRepository;
 import org.apache.coyote.http11.filters.SavedRequestInputFilter;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 // this uses component but this one is more specialised
 public class StudentService {
@@ -18,13 +21,45 @@ public class StudentService {
         this.studentrepo = st ;
     }
 
-    public Student createStudent(Student studentreq )
-    {
+    // POST
+    public Student createStudent(Student studentreq ) {
         System.out.println("student service");
         //Student studentresp = studentrepo.savestudent(studentreq) ;
         Student studentresp = studentrepo.save(studentreq) ; // jpa method save
-
         return studentresp ;
+    }
+    // GET
+    public Student getStudent(Long id ) {
+        Optional<Student> record =  studentrepo.findById(id) ; // this return optional , value may exist or not
+        if(record.isPresent()) return record.get() ;
+        return null ;
+    }
+    // GET ALL
+    public List<Student> getAllStudent() {
+        List<Student> StudentList = studentrepo.findAll() ;
+        return StudentList ;
+    }
+    // UPDATE
+    public Student updateStudent(Long id , Student s){
+        Optional<Student> studentResp = studentrepo.findById(id) ; // check if record exists or not
+        if(studentResp.isEmpty())
+        {
+            return null ;
+        }
+        Student studentToSave = studentResp.get() ;
 
+        studentToSave.setRollno(s.getRollno());
+        studentToSave.setName(s.getName());
+        studentToSave.setSubject(s.getSubject());
+        studentToSave.setEmail(s.getEmail());
+        studentToSave.setAge(s.getAge());
+        return studentrepo.save(studentToSave) ;
+    }
+    // DELETE
+    public Boolean deleteStudent(Long id ) {
+        Boolean isStudent = studentrepo.existsById(id) ;
+        if(!isStudent) return false ;
+        studentrepo.deleteById(id); // it returns nothing
+        return true ;
     }
 }
