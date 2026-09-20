5f424b6282f87d7da364d19691ca56553ba31a34
= <=>
2026-09-20 15:44:00 +0530

    set ID property to auto increment as it is primary key

--- CrudSpringBootDemo/src/main/java/in/amanarmy/CrudSpringBootDemo/entity/Student.java
+++ CrudSpringBootDemo/src/main/java/in/amanarmy/CrudSpringBootDemo/entity/Student.java
@@ -1,6 +1,8 @@
 package in.amanarmy.CrudSpringBootDemo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
@@ -8,6 +10,7 @@ import jakarta.persistence.Id;
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
     private String name ;
     private int age ;
