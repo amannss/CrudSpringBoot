b41f89f66889233bd38208128980b33883d75baf
= <amansharma96609@gmail.com>
2026-09-23 21:53:55 +0530

    added soft-delete Service method using and  reverse soft-delete method

--- CrudSpringBootDemo/src/main/java/in/amanarmy/CrudSpringBootDemo/service/StudentService.java
+++ CrudSpringBootDemo/src/main/java/in/amanarmy/CrudSpringBootDemo/service/StudentService.java
@@ -21,24 +21,33 @@ public class StudentService {
         this.studentrepo = st ;
     }
 
+
     // POST
     public Student createStudent(Student studentreq ) {
-        System.out.println("student service");
+//        System.out.println("student service");
         //Student studentresp = studentrepo.savestudent(studentreq) ;
+        studentreq.setDeleted(false);
         Student studentresp = studentrepo.save(studentreq) ; // jpa method save
         return studentresp ;
     }
+
+
     // GET
     public Student getStudent(Long id ) {
-        Optional<Student> record =  studentrepo.findById(id) ; // this return optional , value may exist or not
+    //  Optional<Student> record =  studentrepo.findById(id) ; // this return optional , value may exist or not
+        Optional<Student> record = studentrepo.findByIdAndDeletedIsFalse(id) ;
         if(record.isPresent()) return record.get() ;
         return null ;
     }
+
+
     // GET ALL
     public List<Student> getAllStudent() {
-        List<Student> StudentList = studentrepo.findAll() ;
+        List<Student> StudentList = studentrepo.findAllByDeletedIsFalse() ;
         return StudentList ;
     }
+
+
     // UPDATE
     public Student updateStudent(Long id , Student s){
         Optional<Student> studentResp = studentrepo.findById(id) ; // check if record exists or not
@@ -53,13 +62,44 @@ public class StudentService {
         studentToSave.setSubject(s.getSubject());
         studentToSave.setEmail(s.getEmail());
         studentToSave.setAge(s.getAge());
+        studentToSave.setDeleted(false);
         return studentrepo.save(studentToSave) ;
     }
+
+
     // DELETE
     public Boolean deleteStudent(Long id ) {
         Boolean isStudent = studentrepo.existsById(id) ;
         if(!isStudent) return false ;
         studentrepo.deleteById(id); // it returns nothing
+        return true ;
+    }
+
+
+    // SOFT DELETE
+    public Boolean deleteStudentSoftly(Long id){
+    //   Optional<Student> isStudent = studentrepo.findById(id) ; // for hard delete
+        Optional<Student> isStudent = studentrepo.findByIdAndDeletedIsFalse(id) ;
+        if(isStudent.isEmpty())
+        {
+            return null ;
+        }
+        // to save in database
+        Student studentToSave = isStudent.get()  ;
+        studentToSave.setDeleted(true);
+        studentrepo.save(studentToSave) ;
+        return true;
+    }
+
+    public Boolean reverseSoftDelete(Long id ){
+        Optional<Student> record = studentrepo.findById(id) ;
+        if(record.isEmpty())
+        {
+            return null ;
+        }
+        Student studentToSave = record.get() ;
+        studentToSave.setDeleted(false);
+        studentrepo.save(studentToSave) ;
         return true ;
     }
 }
